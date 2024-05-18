0d4afb86c0800f4db669c077aa093431bbe675e1
Angel Lopez <a.o.lopez.cruz@icloud.com>
2024-05-18 13:33:18 -0600

    Tablas dos

--- TablasDos/ViewController.swift
+++ TablasDos/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var Model = TableViewModel()
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.dataSource = self
         // Do any additional setup after loading the view.
     }
+}
 
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        Model.getCountArray()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = Model.getTitleArray(index: indexPath)
+        cell?.detailTextLabel?.text = Model.getDescriptionArray(index: indexPath)
+        return cell!
+    }
 }
 
